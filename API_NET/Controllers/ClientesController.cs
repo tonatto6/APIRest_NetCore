1d508c50415f2214d0d1d8c9c521fdaa650cb0c3
tonatto6 <tonattolazaro@gmail.com>
2023-08-31 18:47:28 -0300

    Add clientes controller and endpoint for seek clientes. Add response models

--- API_NET/Controllers/ClientesController.cs
+++ API_NET/Controllers/ClientesController.cs
@@ -1,8 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API_NET.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API_NET.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class ClientesController : ControllerBase
     {
+        List<ClientesModels> clientes;
+
+        //[HttpGet(Name = "GetClientesById")]
+        //public ResponseModels<ClientesModels> getClientesById()
+        //{
+        //    ResponseModels<ClientesModels> response = new ResponseModels<ClientesModels>();
+        //    ClientesModels cliente = new ClientesModels();
+        //    cliente.Nombre = "Lazaro";
+        //    cliente.Apellido = "Tonatto";
+        //    //clientes.Add(cliente);
+
+        //    response.StatusCode = 200;
+        //    response.Body = cliente;
+
+        //    return response;
+        //}
+
+        [HttpGet(Name = "GetClientes")]
+        public ResponseModels<List<ClientesModels>> getClientes()
+        {
+            ResponseModels<List<ClientesModels>> response = new ResponseModels<List<ClientesModels>>();
+            clientes = new List<ClientesModels>();
+            ClientesModels cliente = new ClientesModels();
+            cliente.Nombre = "Lazaro";
+            cliente.Apellido = "Tonatto";
+            clientes.Add(cliente);
+            cliente.Nombre = "Pedro";
+            cliente.Apellido = "Picapiedra";
+            clientes.Add(cliente);
+
+            response.StatusCode = 200;
+            response.Body = clientes;
+
+            return response;
+        }
     }
 }
---
+++ API_NET/Models/ResponseModels.cs
@@ -0,0 +1,9 @@
+﻿namespace API_NET.Models
+{
+    public class ResponseModels<T>
+    {
+        public int StatusCode { get; set; }
+
+        public T Body { get; set; }
+    }
+}
